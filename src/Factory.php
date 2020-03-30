3f9faa3afdf8942c0e3d645b72408fc8bf5b1829
Luke Downing <luke@ricorocks.agency>
2020-03-30 13:44:07 +0100

    Fully implemented Relationship to replace array accessors

--- src/Factory.php
+++ src/Factory.php
@@ -400,8 +400,8 @@ abstract class Factory
                 ->filter(
                     function ($relationships) use ($relationshipMethodName) {
                         return $relationships->filter(
-                            function ($withRelationship) use ($relationshipMethodName) {
-                                return $withRelationship[0] == $relationshipMethodName;
+                            function (Relationship $withRelationship) use ($relationshipMethodName) {
+                                return $withRelationship->getFunctionName() == $relationshipMethodName;
                             }
                         )->isNotEmpty();
                     }
@@ -561,33 +561,31 @@ abstract class Factory
     protected function buildAllWithRelationships($model)
     {
         $this->withRelationships->each(
-            function ($data) use ($model) {
-                $relationshipName = $data[0];
-                $relatedModels = $data[1];
-                $models = $relatedModels instanceof Factory ? $relatedModels->make() : $relatedModels;
+            function (Relationship $relationship) use ($model) {
+                $models = $relationship->getData() instanceof Factory ? $relationship->getData()->make() : $relationship->getData();
 
                 if ($models instanceof Model) {
                     $models = collect([$models]);
                 }
 
                 $models->each(
-                    function ($relatedModel, $index) use ($model, $relationshipName, $relatedModels) {
-                        $model->{$relationshipName}()->save(
+                    function ($relatedModel, $index) use ($model, $relationship) {
+                        $model->{$relationship->getFunctionName()}()->save(
                             $relatedModel,
                             $this->getDesiredAttributeData(
-                                isset($relatedModels->pivotAttributes) ? $relatedModels->pivotAttributes : [],
+                                isset($relationship->getData()->pivotAttributes) ? $relationship->getData()->pivotAttributes : [],
                                 $index
                             )
                         );
 
-                        if ($relatedModels instanceof Factory) {
-                            $relatedModels->buildAllWithRelationships($relatedModel);
+                        if ($relationship->getData() instanceof Factory) {
+                            $relationship->getData()->buildAllWithRelationships($relatedModel);
                         }
                     }
                 );
 
-                if ($relatedModels instanceof Factory) {
-                    $relatedModels->processAfterCreating($models, $model);
+                if ($relationship->getData() instanceof Factory) {
+                    $relationship->getData()->processAfterCreating($models, $model);
                 }
             }
         );
@@ -605,13 +603,13 @@ abstract class Factory
     protected function buildAllForRelationships(Model $model)
     {
         $this->forRelationships->each(
-            function ($data) use ($model) {
-                $relationshipName = $data[0];
-                $cachedLocation = "PoserForRelationship_" . $relationshipName;
+            function (Relationship $relationship) use ($model) {
+                $cachedLocation = "PoserForRelationship_" . $relationship->getFunctionName();
                 if (!isset($this->$cachedLocation)) {
-                    $this->$cachedLocation = $data[1] instanceof Factory ? $data[1]->create() : $data[1];
+                    $this->$cachedLocation = $relationship->getData() instanceof Factory ? $relationship->getData()->create(
+                    ) : $relationship->getData();
                 }
-                $model->{$relationshipName}()->associate($this->$cachedLocation);
+                $model->{$relationship->getFunctionName()}()->associate($this->$cachedLocation);
             }
         );
 
--- src/Relationship.php
+++ src/Relationship.php
@@ -6,7 +6,7 @@ namespace Lukeraymonddowning\Poser;
 
 use ArrayAccess;
 
-class Relationship implements ArrayAccess
+class Relationship
 {
 
     protected $functionName, $data;
@@ -27,56 +27,4 @@ class Relationship implements ArrayAccess
         return $this->data;
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function offsetExists($offset)
-    {
-        return in_array($offset, [0, 1]);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function offsetGet($offset)
-    {
-        switch ($offset) {
-            case 0:
-                return $this->getFunctionName();
-            case 1:
-                return $this->getData();
-        }
-
-        return null;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function offsetSet($offset, $value)
-    {
-        switch ($offset) {
-            case 0:
-                $this->functionName = $value;
-                break;
-            case 1:
-                $this->data = $value;
-                break;
-        }
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function offsetUnset($offset)
-    {
-        switch ($offset) {
-            case 0:
-                $this->functionName = null;
-                break;
-            case 1:
-                $this->data = null;
-                break;
-        }
-    }
 }
